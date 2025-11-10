4f2d76aca4d846b1601295a3f954e723da3da8ec
JLopera10 <jmloperas@eafit.edu.co>
2025-11-10 03:10:58 -0500

    Actualizacion de repo, nueva version codigo

--- cart/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class DireccionEnvioForm(forms.Form):
-    direccion_envio = forms.CharField(label="Dirección de envío", max_length=255, widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Dirección de envío"}))
--- personalizaciones/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PersonalizacionesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'personalizaciones'
